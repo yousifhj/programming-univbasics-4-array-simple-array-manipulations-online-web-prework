2a3ae4e6963f3c75a573cba0cb0708e9c7c3ddc1
Huda Yousif <hjdoski@gmail.com>
2019-09-13 17:50:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,10 +35,12 @@ def using_uniq(array)
   array.uniq()
 end
 
-def using_flatten
+def using_flatten(array)
+  array.flatten()
 end
 
-def using_delete
+def using_delete(array, string)
+  array.delete()
 end
 
 def using_delete_at

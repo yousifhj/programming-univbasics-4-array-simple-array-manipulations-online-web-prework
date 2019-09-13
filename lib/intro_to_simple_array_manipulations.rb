cb6d57a9a8880276a470b41ee7154000874e6422
Huda Yousif <hjdoski@gmail.com>
2019-09-13 19:10:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,6 +43,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer, index, argument1)
+def using_delete_at(array, integer)
   array.sort(using_delete_at, 2)
 end

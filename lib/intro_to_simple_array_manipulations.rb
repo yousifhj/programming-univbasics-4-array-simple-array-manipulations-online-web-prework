1ee4efb37805b6a918e708de7a64b3876b245585
Huda Yousif <hjdoski@gmail.com>
2019-09-13 17:46:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+def using_push(array, string)
+  array.push (string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end 
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat1(array1)
+    using_concat2(array2)
+    using_concat1.concat(array2)
+end
+
+def using_insert(array)
+  array.insert(4)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end

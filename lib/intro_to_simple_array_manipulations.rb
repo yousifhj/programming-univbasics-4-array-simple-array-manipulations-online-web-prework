bbeb7ca7b811288da1d5d99eb25d427a08838d3b
Huda Yousif <hjdoski@gmail.com>
2019-09-13 18:47:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array, more_array)
+def using_concat(array)
+    using_concat(more_arary)
     using_concat.concat(more_array)
 end
 
